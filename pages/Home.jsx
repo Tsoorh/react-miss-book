9b06d1d98871e4e7252bda1174f10570bd659abc
Tsoorh <tsoor.hartov@gmail.com>
2025-08-10 17:44:37 +0300

    Miss-book-starter

---
+++ pages/Home.jsx
@@ -0,0 +1,10 @@
+const { useState, useEffect } = React
+
+export function Home() {
+    return (
+        <section className="home">
+            <h2>Inahal A book</h2>
+       </section>
+    )
+}
+
